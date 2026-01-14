f3c0fc0ad08dd9e45384b3073a41e50dfd9a2662
giahuyn-byte <giahuy.n@kkumail.com>
2026-01-14 22:32:59 +0700

    683040746-8

--- Lab43/P2.py
+++ Lab43/P2.py
@@ -15,8 +15,11 @@ class Person:
         
         self._id = self.__generate_id()
     def __generate_id(self):
-        current_year = datetime.date.today().year
-        new_id = f"{current_year}{Person._id_running_number:03d}"
+        now = datetime.now()
+        self.month = now.month
+        self.year = now.year
+
+        new_id = f"{now}{Person._id_running_number:03d}"
         Person._id_running_number += 1
         return new_id
     def display_info(self):
@@ -25,5 +28,21 @@ class Person:
         print(f"Age: {self.age}")
         print(f"Birthdate: {self._birthdate}")
 class staff(Person):
-    def __init__(self, name, age ):
+    def __init__(self, name, age, department, start_year ):
+        super().__init__(name, age)
+        self.department = department
+        self.start_year = start_year
+        self.__salary = 0
+        self.tenure_year = self.__tenure_year()
+
+    def __tenure_year(self):
+        self.tenure_year = datetime.now().year - self.start_year
+        return self.tenure_year
+    def get_salary(self):
+        return self.__salary
+    
+    def set_salary(self, salary):
+        self.salary = salary 
+
+    def display_info(self):
         
